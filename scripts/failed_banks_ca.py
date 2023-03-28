fa09e13e49c0d5e86c9e8734acaf34e752e2b4d4
ozgeterzioglu <ozgeterzioglu@yahoo.com>
2023-03-28 00:06:22 -0700

    removed an unneccessary for loop clarified a variable name

--- scripts/failed_banks_ca.py
+++ scripts/failed_banks_ca.py
@@ -12,15 +12,14 @@ filtered_banks = []
 
 with open("banklist.csv", "r") as localfile: 
     reader = csv.DictReader(localfile)
-    for banks in reader:
-        if banks["State\xa0"] == "CA":
-            date_data = banks["Closing Date\xa0"]
+    for bank in reader:
+        if bank["State\xa0"] == "CA":
+            date_data = bank["Closing Date\xa0"]
             date_object = datetime.strptime(date_data, "%d-%b-%y")
             date_string = date_object.strftime("%Y-%m-%d")
 
-            for date in banks:
-                banks["Closing Date\xa0"] = date_string
-            filtered_banks.append(banks)
+            bank["Closing Date\xa0"] = date_string
+            filtered_banks.append(bank)
             
 col_headers = ["Bank Name\xa0", "City\xa0", "State\xa0", "Cert\xa0", "Acquiring Institution\xa0", "Closing Date\xa0", "Fund"]
 
@@ -29,5 +28,5 @@ with open("failed_banks_ca.csv", "w") as filteredfile:
     dict_writer.writeheader()
     dict_writer.writerows(filtered_banks)
 
-x = len(filtered_banks)
-print("There are " + str(x) + " failed banks in CA.")
+bank_count = len(filtered_banks)
+print("There are " + str(bank_count) + " failed banks in CA.")
